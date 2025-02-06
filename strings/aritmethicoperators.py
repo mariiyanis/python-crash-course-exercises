35a47ac56c5e5969385a3662f73dfbf8c07343d5
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:09:38 -0300

    Create aritmethicoperators.py

---
+++ strings/aritmethicoperators.py
@@ -0,0 +1,8 @@
+#Exercício 6: Escreva operações de adição, subtração, multiplicação e divisão que resultem no número 8.
+#Lembre-se de colocar suas operações em instruções print para ver os resultados. Você deve criar quatro linhas como esta: print(5 + 3)
+#Sua saída deve simplesmente ser composta de quatro linhas, com o número 8 em cada uma das linhas.
+
+print(5+3)
+print(10-2)
+print(4*2)
+print(80//10)
