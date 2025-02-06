c0ce0e45560a87d9628b98784508cb477e7cdec5
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:10:06 -0300

    Create str().py

---
+++ strings/str().py
@@ -0,0 +1,6 @@
+#Exercício 7: Armazene seu número favorito em uma variável. Em seguida, usando essa variável, crie uma mensagem que revele o seu número favorito.
+#Exiba essa mensagem.
+
+dia_aniversario = 3
+message = "O meu dia favorito é o dia " + str(dia_aniversario) + ", porque é o dia em que eu nasci" + "!"
+print(message)
