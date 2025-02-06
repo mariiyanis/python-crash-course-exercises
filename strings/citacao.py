01381ee764cbae236e8e8f4bf9ee6dbe08c6e867
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:07:12 -0300

    Create citacao.py

---
+++ strings/citacao.py
@@ -0,0 +1,4 @@
+#Exercício 3: Encontre uma citação de uma pessoa famosa que você admire.
+#Exiba a citação e o nome do autor. Sua saída deverá ter a aparência a seguir, incluindo as aspas
+
+print('\tBell Hooks disse uma vez: "Quando largamos o medo, podemos aproximar-nos das pessoas, podemos aproximar-nos da terra,\n\tpodemos aproximar-nos de todas as criaturas celestiais que nos rodeiam."')
