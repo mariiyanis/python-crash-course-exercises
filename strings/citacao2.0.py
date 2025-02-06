bdafdd9290392693d58b0347286d9449835abb98
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:07:48 -0300

    Create citacao2.py

---
+++ strings/citacao2.0.py
@@ -0,0 +1,7 @@
+#Exercício 4: Repita o Exercício 3, porém, desta vez, armazene o nome da pessoa famosa em uma variável chamada famous_person.
+#Em seguida, componha sua mensagem e armazene-a em uma nova variável chamada message. Exiba sua mensagem.
+
+famous_person = "\tBell Hooks"
+message = 'disse uma vez: "Quando largamos o medo, podemos aproximar-nos das pessoas, podemos aproximar-nos da terra,\n\tpodemos aproximar-nos de todas as criaturas celestiais que nos rodeiam."'
+final_message = famous_person + " " + message
+print(final_message)
