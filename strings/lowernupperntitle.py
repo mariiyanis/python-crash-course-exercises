7ee908c53b59a7ba5e12e4e7400744fcdfda9653
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:05:57 -0300

    Create lowernupperntitle.py

---
+++ strings/lowernupperntitle.py
@@ -0,0 +1,7 @@
+#Exercício 2: Armazene o nome de uma pessoa em uma variável
+#e então apresente o nome dessa pessoa em letras minúsculas, em letras maiúsculas e somente com a primeira letra maiúscula.
+
+name = "Son Goku"
+print(name.lower())
+print(name.upper())
+print(name.title())
