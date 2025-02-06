442d34186f275b22fec4b685cbd99b518e15e41d
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 17:04:49 -0300

    Create mensagem.py

---
+++ strings/mensagem.py
@@ -0,0 +1,6 @@
+#Exercício 1: Armazene o nome de uma pessoa em uma variável e apresente uma mensagem a essa pessoa.
+#Sua mensagem deve ser simples, como “Alô Eric, você gostaria de aprender um pouco de Python hoje?”.
+
+name = "Son Goku"
+message = "Hello, " + name + "!" + " Você gostaria de aprender um pouco de Python hoje?"
+print(message)
