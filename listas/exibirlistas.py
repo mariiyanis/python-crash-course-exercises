c6bcb49b4e4f140614c0eb383c5689a13b7f28f8
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:47:02 -0300

    Create exibirlistas.py

---
+++ listas/exibirlistas.py
@@ -0,0 +1,9 @@
+#Exercício9: Trabalhando com um dos programas dos Exercícios de 3.4 a 3.7,
+#use len() para exibir uma mensagem informando o número de pessoas que você está convidando para o jantar
+carros = ['Corolla', 'HB20 Sedan', 'Splint', 'Fiat Palio']
+viagens = ['Hawaii', 'Genipabu', 'Galinhos', 'Bahia', 'Amapá']
+encontro = []
+
+print(len(carros))
+print(len(viagens))
+print(len(encontro))
