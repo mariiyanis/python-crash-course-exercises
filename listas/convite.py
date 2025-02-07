0a1d541f259f7cd9184a09bd6f5af0dca4ffd850
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:28:07 -0300

    Create convite.py

---
+++ listas/convite.py
@@ -0,0 +1,12 @@
+#Exercício 4: Se pudesse convidar alguém, vivo ou morto, para o jantar, quem você convidaria?
+#Crie uma lista que inclua pelo menos três pessoas que você gostaria de convidar para jantar.
+#Em seguida, utilize sua lista para exibir uma mensagem para cada pessoa, convidando-a para jantar.
+
+encontro = ['Liniker', 'Prince', 'Whitney']
+convite1 = "Olá, " + encontro[0] + "! " + "Tudo bem? Você gostaria de tomar um cafezinho da tarde vendo o pôr do sol de Genipabu? Me mande uma resposta. Xêro!"
+convite2 = "Olá, " + encontro[1] + "! " + "Tudo bem? Você gostaria de tomar um cafezinho da tarde vendo o pôr do sol de Genipabu? Me mande uma resposta. Xêro!"
+convite3 = "Olá, " + encontro[2] + "! " + "Tudo bem? Você gostaria de tomar um cafezinho da tarde vendo o pôr do sol de Genipabu? Me mande uma resposta. Xêro!"
+
+print(convite1)
+print(convite2)
+print(convite3)
