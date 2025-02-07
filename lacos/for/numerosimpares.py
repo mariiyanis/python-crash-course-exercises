5ad77c5a63cac3cc13e4d0e523c4be4b9027ccce
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-07 10:15:32 -0300

    Create numerosimpares.py

---
+++ lacos/for/numerosimpares.py
@@ -0,0 +1,6 @@
+#Exercício 6: Use o terceiro argumento da função range() para criar uma lista de números ímpares de 1 a 20.
+#Utilize um laço for para exibir todos os números.
+numeros = list(range(1, 20, 2))
+
+for numero in numeros:
+    print(numero)
