150e6766e6d08b1df1bf9cc90c0b02a0c5ed0a7e
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:27:53 -0300

    Create carros.py

---
+++ listas/carros.py
@@ -0,0 +1,6 @@
+#Exercício 3: Pense em seu meio de transporte preferido, como motocicleta ou carro, e crie uma lista que armazene vários exemplos.
+#Utilize sua lista para exibir uma série de frases sobre esses itens, como “Gostaria de ter uma moto Honda”
+
+carros = ['Corolla', 'HB20 Sedan', 'Splint', 'Fiat Palio']
+mensagem = "Como eu gostaria MUITO de ter um carro " + carros[3] + "!"
+print(mensagem)
