95070a71110d01c50b60264369c226d9d4f5ef44
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:27:25 -0300

    Create message.py

---
+++ listas/message.py
@@ -0,0 +1,6 @@
+#Exercício 2: Comece com a lista usada no Exercício 3.1, mas em vez de simplesmente exibir o nome de cada pessoa, apresente uma mensagem a elas.
+#O texto de cada mensagem deve ser o mesmo, porém cada mensagem deve estar personalizada com o nome da pessoa.
+
+names = ['Fernanda', 'Ricardo', 'Laís', 'Marina', 'Rubens', 'Santos', 'Clara', 'Larissa', 'Rafael', 'Mauricio', 'Manuel', 'Guilherme', 'William']
+message = "Just keep doing your best just the way you've been doing all this time.\nI truly believe and trust you, " + names[0] + "."
+print(message)
