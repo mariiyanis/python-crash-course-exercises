e21c59173b1356249e47494520366ba5e5110b47
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:26:58 -0300

    Create amigos.py

---
+++ listas/amigos.py
@@ -0,0 +1,31 @@
+#Exercício 1: Armazene os nomes de alguns de seus amigos em uma lista chamada names.
+#Exiba o nome de cada pessoa acessando cada elemento da lista, um de cada vez.
+
+names = ['Fernanda', 'Ricardo', 'Laís', 'Marina', 'Rubens', 'Santos', 'Clara', 'Larissa', 'Rafael', 'Mauricio', 'Manuel', 'Guilherme', 'William']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
+print(names[7])
+print(names[8])
+print(names[9])
+print(names[10])
+print(names[11])
+print(names[-1])
+
+#Em ordem DECRESCENTE
+print(names[-2])
+print(names[-3])
+print(names[-4])
+print(names[-5])
+print(names[-6])
+print(names[-7])
+print(names[-8])
+print(names[-9])
+print(names[-10])
+print(names[-11])
+print(names[-12])
+print(names[-13])
