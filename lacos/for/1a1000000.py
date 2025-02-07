fd7ede885c28640eea97c15ca85dc697a09b2cb8
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-07 10:14:54 -0300

    Create 1a1000000.py

---
+++ lacos/for/1a1000000.py
@@ -0,0 +1,5 @@
+#Exercício 4: Crie uma lista de números de um a um milhão e, então, use um laço for para exibir os números.
+numeros = list(range(1,1000001))
+
+for numero in numeros:
+    print(numero)
