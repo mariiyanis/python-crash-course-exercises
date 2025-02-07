8dea90645beaa12d95850eb8da4d5fa1d94ef6dc
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-07 10:17:42 -0300

    Create multiplosdetres.py

---
+++ lacos/for/multiplosdetres.py
@@ -0,0 +1,4 @@
+#Exercício 7: Crie uma lista de múltiplos de 3, de 3 a 30. Use um laço for para exibir os números de sua lista.
+multiplos_de_3 = list(range(3, 33, 3))
+for multiplos in multiplos_de_3:
+    print(multiplos)
