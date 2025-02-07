3731afcbb68ae641554a89a22b74ef34861f3e69
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-06 21:28:54 -0300

    Create mudandoconvites.py

---
+++ mudandoconvites.py
@@ -0,0 +1,12 @@
+#Exercício 5: Você acabou de saber que um de seus convidados não poderá comparecer ao jantar, portanto será necessário enviar um novo
+#Conjunto de convites. Você deverá pensar em outra pessoa para convidar.
+#Comece com seu programa do Exercício 3.4.
+#Acrescente uma instrução print no final de seu programa, especificando o nome do convidado que não poderá comparecer.
+#Modifique sua lista, substituindo o nome do convidado que não poderá comparecer pelo nome da nova pessoa que você está convidando.
+#Exiba um segundo conjunto de mensagens com o convite, uma para cada pessoa que continua presente em sua lista.
+
+encontro = ['Liniker', 'Prince', 'Whitney']
+print(encontro[-1])
+
+encontro[-1] = 'Chaka Khan'
+print(encontro)
