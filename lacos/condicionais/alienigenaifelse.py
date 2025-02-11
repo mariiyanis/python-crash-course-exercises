edeebb3cb19fd27038299cf4428bf321487f04f0
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-11 16:49:11 -0300

    Create alienigenaifelse.py

---
+++ lacos/condicionais/alienigenaifelse.py
@@ -0,0 +1,15 @@
+#Exercício 4: Escolha uma cor para um alienígena, como foi feito no Exercício 5.3, e escreva uma cadeia if-else.
+#Se a cor do alienígena for verde, mostre uma frase informando que o jogador acabou de ganhar cinco pontos por atingir o alienígena.
+#Se a cor do alienígena não for verde, mostre uma frase informando que o jogador acabou de ganhar dez pontos.
+#Escreva uma versão desse programa que execute o bloco if e outro que execute o bloco else.
+cor_alienigena = 'Amarelo'
+if cor_alienigena == 'Verde':
+    print("O jogador acabou de ganhar cinco pontos por atingir o alienígena!")
+else:
+    print("O jogador acabou de ganhar dez pontos!")
+
+cor_alienigena = 'Verde'
+if cor_alienigena == 'Verde':
+    print("O jogador acabou de ganhar cinco pontos por atingir o alienígena!")
+else:
+    print("O jogador acabou de ganhar dez pontos!")
