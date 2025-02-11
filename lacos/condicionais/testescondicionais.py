a62c9903d9bfb10c932a70a12a286f2055affea4
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-11 16:48:04 -0300

    Create testescondicionais.py

---
+++ lacos/condicionais/testescondicionais.py
@@ -0,0 +1,29 @@
+#Exercício 1: Escreva uma série de testes condicionais.
+#Exiba uma frase que descreva o teste e o resultado previsto para cada um.
+#Seu código deverá ser semelhante a:
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+#Observe atentamente seus resultados e certifique-se de que compreende por que cada linha é avaliada como True ou False.
+#Crie pelo menos dez testes. Tenha no mínimo cinco testes avaliados como True e outros cinco avaliados como False.
+moto = 'honda'
+print("\nIs moto == 'honda'? I predict True.")
+print(moto == 'honda')
+
+print("\nIs moto == 'yamaha'? I predict False.")
+print(moto == 'yamaha')
+
+language = 'portuguese'
+print("Is portuguese == language? I predict True.")
+print(language == 'portuguese')
+
+sport = 'volleyball'
+print("Is volleyball == sport? I predict False.")
+print(sport == 'volleyball')
+
+pizza = 'lombo com carne de sol na nata'
+print("Is pizza == pepperoni? I predict True.")
+print(pizza == 'pepperoni')
