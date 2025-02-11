c4a572b273f4db72e26e50e0a39e1a80a5b01578
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-11 16:48:50 -0300

    Create alienigenas.py

---
+++ lacos/condicionais/alienigenas.py
@@ -0,0 +1,14 @@
+#Exercício 3: Suponha que um alienígena acabou de ser atingido em um jogo.
+#Crie uma variável chamada alien_color e atribua-lhe um valor igual a 'green', 'yellow' ou 'red'.
+#Escreva uma instrução if para testar se a cor do alienígena é verde.
+#Se for, mostre uma mensagem informando que o jogador acabou de ganhar cinco pontos.
+#Escreva uma versão desse programa em que o teste if passe e outro em que ele falhe. (A versão que falha não terá nenhuma saída.)
+alien_color = 'green'
+if alien_color == 'green':
+    print("O jogador acabou de ganhar cinco pontos!")
+
+alien_color = 'black'
+if alien_color == 'green':
+    print("O jogador acabou de ganhar cinco pontos!")
+else:
+    print()
