673c4a52c83892f16464d05daaaf1de32b193588
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-12 10:58:30 -0300

    Create frutas.py

---
+++ lacos/condicionais/frutas.py
@@ -0,0 +1,23 @@
+frutas = ['Mamão', 'Banana', 'Abacaxi', 'Maçã', 'Uva']
+if 'Mamão' in frutas:
+    print(True)
+if 'Banana' in frutas:
+    print(True)
+if 'Abacaxi' in frutas:
+    print(True)
+if 'Maçã' in frutas:
+    print(True)
+if 'Uva' in frutas:
+    print(True)
+
+frutas_favoritas = ['Mamão', 'Abacaxi', 'Maçã']
+if 'Mamão' in frutas_favoritas:
+    print("Você realmente gosta dessa fruta!")
+if 'Banana' in frutas_favoritas:
+    print("Você realmente gosta dessa fruta!")
+if 'Abacaxi' in frutas_favoritas:
+    print("Você realmente gosta dessa fruta!")
+if 'Maçã' in frutas_favoritas:
+    print("Você realmente gosta dessa fruta!")
+if 'Uva' in frutas_favoritas:
+    print("Você realmente gosta dessa fruta!")
