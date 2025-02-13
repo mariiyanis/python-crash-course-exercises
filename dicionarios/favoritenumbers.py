f91241c35200107e65e3f28201570a4f592bb553
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-13 11:34:54 -0300

    Create favoritenumbers.py

---
+++ dicionarios/favoritenumbers.py
@@ -0,0 +1,11 @@
+#Exercício 2: Números favoritos: Use um dicionário para armazenar os números favoritos de algumas pessoas.
+#Pense em cinco nomes e use-os como chaves em seu dicionário. Pense em um número favorito para cada pessoa e armazene cada um
+#como um valor em seu dicionário. Exiba o nome de cada pessoa e seu número favorito.
+#Para que seja mais divertido ainda, faça uma enquete com alguns amigos e obtenha alguns dados reais para o seu programa.
+
+favoritenumbers = {'Clarissa': 7, 'Jorge': 1, 'Emanuel': 9, 'Arthur': 3, 'Sophia': 5}
+print("O número favorito de Sophia é " + str(favoritenumbers['Sophia']) + "!")
+print("O número favorito de Clarissa é " + str(favoritenumbers['Clarissa']) + "!")
+print("O número favorito de Jorge é " + str(favoritenumbers['Jorge']) + "!")
+print("O número favorito de Emanuel é " + str(favoritenumbers['Emanuel']) + "!")
+print("O número favorito de Arthur é " + str(favoritenumbers['Arthur']) + "!")
