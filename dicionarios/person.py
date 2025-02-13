248497f6315ba611c77ad644fbf76a3d34a7b165
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-13 11:34:34 -0300

    Update person.py

--- dicionarios/person.py
+++ dicionarios/person.py
@@ -1,1 +1,9 @@
+#Exercício 1: Use um dicionário para armazenar informações sobre uma pessoa que você conheça.
+#Armazene seu primeiro nome, o sobrenome, a idade e a cidade em que ela vive.
+#Você deverá ter chaves como first_name, last_name, age e city.
+#Mostre cada informação armazenada em seu dicionário.
 
+tarsoslife = {'first_name': 'tarso', 'last_name': 'bittencourt', 'age': '39', 'city': 'Manhattan'}
+print("Nome Completo: " + tarsoslife['first_name'] + " " + tarsoslife['last_name'])
+print("Idade: " + str(tarsoslife['age']) + " anos")
+print("Cidade: " + tarsoslife['city'])
