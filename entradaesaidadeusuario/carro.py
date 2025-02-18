585f59878967bd6a75dbf3497cdd9d71e270fcf5
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-18 07:27:33 -0300

    Create carro.py

---
+++ entradaesaidadeusuario/carro.py
@@ -0,0 +1,6 @@
+#Exercício 1:  Escreva um programa que pergunte ao usuário qual tipo de carro ele gostaria de alugar.
+#Mostre uma mensagem sobre esse carro, por exemplo, “Deixe-me ver se consigo um Subaru para você.”
+
+car = input("Qual carro você gostaria de alugar?")
+
+print(f"\nDeixe-me ver se consigo um {car} para você.")
