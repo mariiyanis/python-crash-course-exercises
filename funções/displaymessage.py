6720162d8cbce8770ef6d068677127ef33b71fd1
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2025-02-28 20:12:22 -0300

    Create displaymessage.py

---
+++ funções/displaymessage.py
@@ -0,0 +1,8 @@
+#Exercício 1: Escreva uma função chamada display_message() que mostre uma frase informando a todos
+#o que você está aprendendo neste capítulo. Chame a função e certifique-se de que a mensagem seja exibida corretamente.
+
+def display_message():
+    """ Exibe uma mensagem explicando o que vou estudar agora! """
+    print("Olá! estou estudando funções em python!!!!")
+
+display_message():
